2b9002aab208e9792434dc6c53fb222c9475d2d0
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-02-08 15:25:58 +0100

    FLAM-19 managing the overall operations the members can do on a post (updating and deleting), [files modified=>blogController.js, blog.js in the routes folder]

--- API/src/controllers/blogController.js
+++ API/src/controllers/blogController.js
@@ -1,25 +1,6 @@
 const Blog = require('../models/BlogModel')
 const ErrorHandler = require('../utils/errorHandler')
 
-
-exports.createPost = async (req, res, next) =>{
-    try {
-        const data ={
-            title: req.body.title,
-            postPicture: req.body.postPicture,
-            content: req.body.content,
-            creator: req.user._id
-        };
-        const post = await Blog.create(data);
-        res.status(201).json({
-            success: true,
-            post
-        })
-    } catch (error) {
-        next(error)
-    }
-}
-
 exports.getAllPosts = async (req, res, next) =>{
     try{
         const posts = await Blog.find()
@@ -43,6 +24,64 @@ exports.getPostById = async(req, res, next)=>{
         res.status(200).json({
             success: true,
             post
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.createPost = async (req, res, next) =>{
+    try {
+        const data ={
+            title: req.body.title,
+            postPicture: req.body.postPicture,
+            content: req.body.content,
+            creator: req.user._id
+        };
+        const post = await Blog.create(data);
+        res.status(201).json({
+            success: true,
+            post
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.updatePost = async (req, res, next)=>{
+    try {
+        const postId = req.params.id
+        const currentPost = await Blog.findOne({_id:postId})
+        const data = {
+            title: req.body.title || currentPost.title,
+            postPicture: req.body.postPicture || currentPost.postPicture,
+            content: req.body.content || currentPost.content,
+        }
+        const updatedPost = await Blog.findOneAndUpdate(
+            {_id: postId},
+            data,
+            {new: true}
+        )
+        res.status(200).json({
+            success: true,
+            updatedPost,
+          });
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.deletePost = async (req, res, next)=>{
+    try {
+        const postId = req.params.id
+
+        const post = await Blog.findOneAndDelete({_id: postId})
+        if(!post){
+            return next(new ErrorHandler('post doesnt exist', 404))
+        }
+        res.status(200).json({
+            success: true,
+            message: 'post deleted successfully !'
         })
     } catch (error) {
         next(error)
--- API/src/routes/blog.js
+++ API/src/routes/blog.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const { createPost, getAllPosts, getPostById } = require('../controllers/blogController')
+const { createPost, getAllPosts, getPostById, deletePost, updatePost } = require('../controllers/blogController')
 const { authenticate, isAllowedToCreateArticles } = require('../middlewares/authentication')
 const router = express.Router()
 
 router.post('/create', authenticate, isAllowedToCreateArticles, createPost)
-router.put('/update/:id')
-router.delete('/delete/:id')
+router.put('/update/:id', authenticate, isAllowedToCreateArticles, updatePost)
+router.delete('/delete/:id', authenticate, isAllowedToCreateArticles, deletePost)
 router.get("/allposts", getAllPosts)
 router.get("/post/:id", getPostById)
 
