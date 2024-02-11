13b354e0655cba167ba8d1d8afe8c71b204c2c2a
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-02-11 17:07:41 +0100

    FLAM-39 Admin Can create, update and delete categories, [files Updated: categoryController.js in the controllers folder, category.js in the routes folder]

--- API/src/controllers/categoryController.js
+++ API/src/controllers/categoryController.js
@@ -29,3 +29,65 @@ exports.getCategoryById = async(req, res, next)=>{
         next(error)
     }
 }
+
+
+exports.createCategory = async (req, res, next) =>{
+    try {
+        const data ={
+            name: req.body.name,
+            description: req.body.description,
+            categoryPicture: req.body.categoryPicture,
+        };
+        const category = await Category.create(data);
+        res.status(201).json({
+            success: true,
+            category
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+exports.updateCategory = async (req, res, next)=>{
+    try {
+        const categoryId = req.params.id
+        const currentCategory= await Category.findOne({_id:categoryId})
+        if(!currentCategory){
+            return next(new ErrorHandler('category doesnt exist', 404))
+        }
+        const data = {
+            name: req.body.name || currentCategory.name,
+            description: req.body.description || currentCategory.description,
+            categoryPicture: req.body.categoryPicture || currentCategory.categoryPicture,
+        }
+        const updatedCategory = await Category.findOneAndUpdate(
+            {_id: categoryId},
+            data,
+            {new: true}
+        )
+        res.status(200).json({
+            success: true,
+            updatedCategory,
+          });
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.deleteCategory = async (req, res, next)=>{
+    try {
+        const categoryId = req.params.id
+
+        const category = await Category.findOneAndDelete({_id: categoryId})
+        if(!category){
+            return next(new ErrorHandler('category doesnt exist', 404))
+        }
+        res.status(200).json({
+            success: true,
+            message: 'catgeory deleted successfully !'
+        })
+    } catch (error) {
+        next(error)
+    }
+}
--- API/src/routes/category.js
+++ API/src/routes/category.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const { getAllCategories, getCategoryById } = require('../controllers/categoryController')
+const { 
+    getAllCategories, 
+    getCategoryById, 
+    createCategory, 
+    updateCategory, 
+    deleteCategory 
+} = require('../controllers/categoryController')
+const { 
+    authenticate, 
+    isAdmin 
+} = require('../middlewares/authentication')
 
+router.post('/create', authenticate, isAdmin, createCategory)
+router.put('/update/:id', authenticate, isAdmin, updateCategory)
+router.delete("/delete/:id", authenticate, isAdmin, deleteCategory)
 
 router.get('/allCategories', getAllCategories)
 router.get('/category/:id', getCategoryById)
