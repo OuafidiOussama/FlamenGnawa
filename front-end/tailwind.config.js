d1d5ff81173110db10b7f5b7b33a60cd6fdf2de6
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-03-17 15:01:12 +0000

    Installing all the dependacies needed

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      'main': ['Lemon'],
+      'arabic': ['Lemonada'],
+      'description' :['Lexend'],
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
