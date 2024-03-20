1ed9da086b10b0fc6f2408c45aefc2e3083604ae
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-03-20 23:05:57 +0000

    Creating the necessary pages for the Members

---
+++ front-end/src/pages/admin/members/MemberForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MemberForm() {
+  return (
+    <div>MemberForm</div>
+  )
+}
---
+++ front-end/src/pages/admin/members/MembersTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MembersTable() {
+  return (
+    <div>MembersTable</div>
+  )
+}
