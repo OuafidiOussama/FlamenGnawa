0adcbd68528194efbc62192ec53bbbd2cca99c3f
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-03-20 23:05:44 +0000

    Creating the necessary pages for the Events

---
+++ front-end/src/pages/admin/events/EventFrom.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EventFrom() {
+  return (
+    <div>EventFrom</div>
+  )
+}
---
+++ front-end/src/pages/admin/events/EventsTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EventsTable() {
+  return (
+    <div>EventsTable</div>
+  )
+}
