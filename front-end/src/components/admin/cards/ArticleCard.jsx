0766b0e3f6838ba7e4b7dfdb48bf5f0a225f0bc0
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-03-23 22:39:40 +0000

    Creating the blog table page for the dashboard + Article card to hold data to be displayed

---
+++ front-end/src/components/admin/cards/ArticleCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import logo from "../../../assets/Final.png";
+import { Icon } from "@iconify/react";
+
+export default function ArticleCard() {
+  return (
+    <tr className="h-20 border-white border-b-2">
+      <td className="flex justify-center items-center h-20 w-28">
+        <img
+          src={logo}
+          alt=""
+          className="w-16 h-16 object-cover rounded-full"
+        />
+      </td>
+      <td className="w-40">Hello</td>
+      <td className="truncate w-full">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem in
+        quidem nesciunt atque ipsum quisquam architecto debitis repudiandae
+        doloremque enim. Repellat libero cupiditate natus aut fuga doloremque,
+        dicta est quisquam? lor
+      </td>
+      <td className="w-40">Oussama OUafidi</td>
+      <td className="w-40">15 / 10 / 2015</td>
+      <td className="w-48">
+        <div className="flex justify-between px-2 text-white">
+          <button className="flex bg-in-stock py-2 px-2 rounded items-center gap-2">
+            <Icon icon="lucide:edit" className="text-2xl" />
+          </button>
+          <button className="flex bg-out-of-stock py-2 px-2 rounded items-center gap-2">
+            <Icon icon="ic:baseline-delete" className="text-2xl" />
+          </button>
+        </div>
+      </td>
+    </tr>
+  )
+}
--- front-end/src/pages/admin/blog/BlogTable.jsx
+++ front-end/src/pages/admin/blog/BlogTable.jsx
@@ -1,7 +1,36 @@
+import { Icon } from '@iconify/react'
 import React from 'react'
+import ArticleCard from '../../../components/admin/cards/ArticleCard'
 
 export default function BlogTable() {
   return (
-    <div>BlogTable</div>
+    <div className="px-10 py-5 h-screen overflow-hidden">
+      <div className="bg-border/20 h-full rounded-xl flex flex-col p-3 gap-5">
+        <div className="border-b-4 py-1 border-border h-14 flex justify-between">
+          <p className="font-bold text-4xl">Members</p>
+          <button className="flex items-center gap-2 bg-purple text-white py-2 px-5 rounded">
+            <Icon icon="basil:add-solid" className="text-2xl" /> Add Article
+          </button>
+        </div>
+
+        <div className="w-full h-full bg-black/10 rounded-xl overflow-y-auto">
+          <table className="w-full table-fixed text-center">
+            <thead className="border-white border-b-2 h-10">
+              <tr>
+                <th className="w-20">Picture</th>
+                <th className="w-20 text-center">Title</th>
+                <th className="w-96 overflow-hidden">Content</th>
+                <th className="w-20">Creator</th>
+                <th className="w-20">Created At</th>
+                <th className="w-20">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ArticleCard />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
