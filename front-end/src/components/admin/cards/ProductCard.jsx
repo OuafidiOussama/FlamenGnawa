27c51a373ce2d181ca825cae6e8d5503b5a0933c
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-03-23 22:46:50 +0000

    Creating the shop table for the store + Product Card

--- front-end/src/components/admin/cards/ProductCard.jsx
+++ front-end/src/components/admin/cards/ProductCard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react'
+import { Icon } from '@iconify/react'
 import logo from "../../../assets/Final.png";
-import { Icon } from "@iconify/react";
 
-export default function MembersCard() {
+export default function ProductCard() {
   return (
     <tr className="h-20 border-white border-b-2">
       <td className="flex justify-center items-center h-20 w-28">
@@ -12,14 +12,17 @@ export default function MembersCard() {
           className="w-16 h-16 object-cover rounded-full"
         />
       </td>
-      <td className="w-40">Malcolm Lockyer</td>
-      <td className="truncate w-full">
+      <td className="w-40">Bass guitar</td>
+      <td className="truncate w-80">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem in
         quidem nesciunt atque ipsum quisquam architecto debitis repudiandae
         doloremque enim. Repellat libero cupiditate natus aut fuga doloremque,
         dicta est quisquam? lor
       </td>
-      <td className="w-40">1961</td>
+      <td className="w-40">Unknown</td>
+      <td className="w-40">400</td>
+      <td className="w-40">350dhs</td>
+      <td className="w-40">Guitars</td>
       <td className="w-48">
         <div className="flex justify-between px-2 text-white">
           <button className="flex bg-in-stock py-2 px-2 rounded items-center gap-2">
@@ -31,5 +34,5 @@ export default function MembersCard() {
         </div>
       </td>
     </tr>
-  );
+  )
 }
--- front-end/src/pages/admin/shop/ShopTable.jsx
+++ front-end/src/pages/admin/shop/ShopTable.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Icon } from '@iconify/react'
+import ProductCard from '../../../components/admin/cards/ProductCard'
 
 export default function ShopTable() {
   return (
-    <div>ShopTable</div>
+    <div className="px-10 py-5 h-screen overflow-hidden">
+      <div className="bg-border/20 h-full rounded-xl flex flex-col p-3 gap-5">
+        <div className="border-b-4 py-1 border-border h-14 pr- flex justify-between">
+          <p className="font-bold text-4xl">Events</p>
+          <button className="flex items-center gap-2 bg-purple text-white py-2 px-5 rounded">
+            <Icon icon="basil:add-solid" className="text-2xl" /> Add Event
+          </button>
+        </div>
+
+        <div className="w-full h-full bg-black/10 rounded-xl overflow-y-auto">
+          <table className="w-full table-fixed text-center">
+            <thead className="border-white border-b-2 h-10">
+              <tr>
+                <th className="w-20">Picture</th>
+                <th className="w-20 text-center">Label</th>
+                <th className="w-52 overflow-hidden">Desciption</th>
+                <th className="w-20">Brand</th>
+                <th className="w-20">Quanity</th>
+                <th className="w-20">Price</th>
+                <th className="w-20">Category</th>
+                <th className="w-20">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <ProductCard />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
