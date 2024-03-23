ced6e3f5c0a6e850073845fd7cbe64fd60f4f051
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-03-23 22:33:40 +0000

    Making the event table page for the dashboard

--- front-end/src/pages/admin/events/EventsTable.jsx
+++ front-end/src/pages/admin/events/EventsTable.jsx
@@ -1,7 +1,38 @@
+import { Icon } from '@iconify/react'
 import React from 'react'
+import EventsCard from '../../../components/admin/cards/EventsCard'
 
 export default function EventsTable() {
   return (
-    <div>EventsTable</div>
+    <div className="px-10 py-5 h-screen overflow-hidden">
+      <div className="bg-border/20 h-full rounded-xl flex flex-col p-3 gap-5">
+        <div className="border-b-4 py-1 border-border h-14 pr- flex justify-between">
+          <p className="font-bold text-4xl">Events</p>
+          <button className="flex items-center gap-2 bg-purple text-white py-2 px-5 rounded">
+            <Icon icon="basil:add-solid" className="text-2xl" /> Add Event
+          </button>
+        </div>
+
+        <div className="w-full h-full bg-black/10 rounded-xl overflow-y-auto">
+          <table className="w-full table-fixed text-center">
+            <thead className="border-white border-b-2 h-10">
+              <tr>
+                <th className="w-20">Picture</th>
+                <th className="w-20 text-center">title</th>
+                <th className="w-52 overflow-hidden">Desciption</th>
+                <th className="w-20">Date</th>
+                <th className="w-20">Time</th>
+                <th className="w-20">Location</th>
+                <th className="w-20">Price</th>
+                <th className="w-20">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <EventsCard />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
