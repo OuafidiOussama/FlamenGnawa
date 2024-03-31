7b69206b6c1aaec5a6d80504f905a34fc65803a7
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-03-31 21:51:03 +0000

    Increasing the update limit to 5mb

--- API/src/app.js
+++ API/src/app.js
@@ -7,7 +7,7 @@ const errorHandler = require("./middlewares/error");
 const connectDB = require("./config/db");
 connectDB();
 
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "5mb" }));
 app.use(cors());
 app.use(errorHandler);
 
