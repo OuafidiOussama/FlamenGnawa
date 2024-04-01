809734ee1bafb94e3dfe82de544c9b1a2b963d60
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-04-01 11:49:19 +0000

    adding the timestamps field so i can sort event by latest

--- API/src/controllers/eventController.js
+++ API/src/controllers/eventController.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 exports.getAllEvents = async (req, res, next) => {
   try {
-    const Events = await Event.find()
+    const Events = await Event.find().sort({createdAt: -1})
     res.status(200).json({
       success: true,
       Events,
--- API/src/models/EventModel.js
+++ API/src/models/EventModel.js
@@ -1,39 +1,41 @@
 const mongoose = require("mongoose");
 
-const eventSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    trim: true,
-    required: [true, "Please Provide Your Event Title"],
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please Provide Your Event Title"],
+    },
+    eventPicture: {
+      type: String,
+      trim: true,
+      required: [true, "Please Provide Your Event Picture"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "Please Provide Your Event Description"],
+    },
+    eventDate: {
+      type: Date,
+      required: [true, "Please Provide Your Event Date"],
+    },
+    location: {
+      type: String,
+      trim: true,
+      required: [true, "Please Provide Your Event Location"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please Provide Your Event Price"],
+    },
+    tickets: {
+      type: Number,
+      required: [true, "Please Provide Your Available Tickets Number"],
+    },
   },
-  eventPicture: {
-    type: String,
-    trim: true,
-    required: [true, "Please Provide Your Event Picture"],
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, "Please Provide Your Event Description"],
-  },
-  eventDate: {
-    type: Date,
-    required: [true, "Please Provide Your Event Date"],
-  },
-  location: {
-    type: String,
-    trim: true,
-    required: [true, "Please Provide Your Event Location"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Please Provide Your Event Price"],
-  },
-  tickets: {
-    type: Number,
-    required: [true, "Please Provide Your Available Tickets Number"],
-  },
-
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("events", eventSchema);
