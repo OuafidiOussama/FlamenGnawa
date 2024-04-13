2e87bad88dbfeb93287deab98f19ec3f4ef65f32
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-04-13 00:04:29 +0000

    url change

--- front-end/src/redux/axios.js
+++ front-end/src/redux/axios.js
@@ -9,7 +9,7 @@ function extractToken(){
 }
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://flamengnawa-server.onrender.com",
     headers: {
         "Content-Type": 'application/json',
         "Authorization": `Bearer ${extractToken()}`
